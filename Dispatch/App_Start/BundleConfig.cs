674d96539326e291932a78c932d018b6d603d961
Marlon Lira <marlonlira3@gmail.com>
2019-06-06 10:51:03 -0300

    att2

---
+++ Dispatch/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI;
+
+namespace Dispatch {
+    public class BundleConfig {
+        // Para obter mais informações sobre o Agrupamento, visite https://go.microsoft.com/fwlink/?LinkID=303951
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/bundles/Js").Include(
+                            "~/Scripts/jquery-3.3.1.min.js",
+                            "~/Scripts/bootstrap.min.js",
+                            "~/Scripts/moment.min.js",
+                            "~/Scripts/Chart.min.js",
+                            "~/Scripts/tooplate-scripts.js"
+                            ).IncludeDirectory("~/Scripts",".js"));
+
+            bundles.Add(new StyleBundle("~/bundles/Css").Include(
+                            "~/Content/bootstrap.min.css",
+                            "~/Content/templatemo-style.css"
+                            ).IncludeDirectory("~/Content", ".css"));
+
+        }
+    }
+}
---
+++ Dispatch/Context/ConnectDb.cs
@@ -0,0 +1,57 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Entity;
+using Dispatch.Models;
+
+namespace Dispatch.Context {
+    public class ConnectDb : DbContext{
+
+        //testar
+	    public ConnectDb()
+        : base("gestor2"){
+
+        }
+        public DbSet<AlunoMdl> Alunos { get; set; }
+
+        // Conexão sql 
+        public string CreateCon = ConfigurationManager.ConnectionStrings["gestor2"].ConnectionString;
+        public string CreateWardenCon = ConfigurationManager.ConnectionStrings["Campaign"].ConnectionString;
+
+        public SqlConnection Con;
+        public SqlCommand Cmd;
+        public SqlDataAdapter Adapt;
+
+        //Conexão banco
+
+        public void OpenCon() {
+            Con = new SqlConnection(CreateCon);
+            Cmd = new SqlCommand();
+            Cmd.CommandType = CommandType.Text;
+            Cmd.CommandTimeout = 600;
+            Con.Open();
+        }
+        /*
+        public void OpenConlocal() {
+            Con = new SqlConnection(CreateCon2);
+            Con.Open();
+        }*/
+
+        public void OpenConWarden() {
+            Con = new SqlConnection(CreateWardenCon);
+            Cmd = new SqlCommand();
+            Cmd.CommandType = CommandType.Text;
+            Cmd.CommandTimeout = 600;
+            Con.Open();
+        }
+        
+        public void OpenAdpter(string tabela) {
+            Adapt = new SqlDataAdapter(tabela, Con);
+            Adapt.SelectCommand.CommandTimeout = 600;
+        }
+
+        public void CloseCon() {
+            Con.Close();
+        }
+    }
+}
